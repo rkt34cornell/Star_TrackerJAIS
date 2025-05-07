017a6d03696a30c90fccfd4b4e10b066a4a6eb48
rkt34cornell <146210016+rkt34cornell@users.noreply.github.com>
2025-05-07 18:30:26 -0400

    Update stt.py

--- RPi/stt.py
+++ RPi/stt.py
@@ -2,28 +2,25 @@ import argparse
 import stt_selector as stt
 
 parser = argparse.ArgumentParser(prog="STT", description="STT main script")
-parser.add_argument("type", type=str,
-                    help="The way of running the software: direct_rpi, sample_rpi, sample_stereo.")
-parser.add_argument("catalog", type=int,
-                    help="The degree of catalog separation in the algorithm: 5, 10, 15.")
-parser.add_argument("-exp", "--exptime", type=int,
-                    help="Exposure time (in ms) of the grabbed picture (in direct mode).", default=800)
-parser.add_argument("-n", "--npic", type=int,
-                    help="The number of the picture to analyze (in sample mode).", default=1)
+parser.add_argument("type", type=str, choices=["direct_rpi", "sample_rpi", "sample_stereo", "custom"],
+                    help="Running mode: direct_rpi, sample_rpi, sample_stereo, or custom")
+parser.add_argument("catalog", type=int, help="Catalog division: 5, 10, or 15")
+parser.add_argument("-exp", "--exptime", type=int, default=800, help="Exposure time for direct mode")
+parser.add_argument("-n", "--npic", type=int, default=1, help="Image number for sample modes")
+parser.add_argument("-f", "--file", type=str, help="Full path to custom .fts file")
+
 args = parser.parse_args()
-stt_type = args.type
-cat_division = args.catalog
-n_pic = args.npic
-exp_time = args.exptime
 
-if cat_division not in (5, 10, 15):
-    parser.error("Please introduce a valid catalog division: 5/10/15")
+if args.catalog not in (5, 10, 15):
+    parser.error("Catalog division must be 5, 10, or 15")
 
-if stt_type == "direct_rpi":
-    stt.solve_lis_grab_img(cat_division, exp_time)
-elif stt_type == "sample_rpi":
-    stt.solve_lis_sample_rpi(cat_division, n_pic)
-elif stt_type == "sample_stereo":
-    stt.solve_lis_sample_stereo(cat_division, n_pic)
-else:
-    raise NameError("---> ERROR: Please introduce a valid option to use this STT software (see the Documentation)!")
+if args.type == "direct_rpi":
+    stt.solve_lis_grab_img(args.catalog, args.exptime)
+elif args.type == "sample_rpi":
+    stt.solve_lis_sample_rpi(args.catalog, args.npic)
+elif args.type == "sample_stereo":
+    stt.solve_lis_sample_stereo(args.catalog, args.npic)
+elif args.type == "custom":
+    if not args.file:
+        parser.error("Please specify the file path using --file for custom mode.")
+    stt.solve_lis_custom(args.catalog, args.file)
